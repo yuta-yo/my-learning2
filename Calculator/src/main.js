23be44265fce6e076187d5c0c09a60266707c652
吉田雄多 <yuta@MacBook-Air-3.local>
2022-10-31 12:38:38 +0900

    掛け算の実装

--- Calculator/src/main.js
+++ Calculator/src/main.js
@@ -13,70 +13,131 @@ var OPERATOR;
 (function (OPERATOR) {
     OPERATOR["PLUS"] = "plus";
     OPERATOR["MINUS"] = "minus";
+    OPERATOR["MULTIPLY"] = "multiply";
 })(OPERATOR || (OPERATOR = {}));
 var calcItem = {
     leftOperand: "",
+    centerOperand: "",
     rightOperand: "",
-    operator: ""
+    leftOperator: "",
+    rightOperator: ""
 };
 var display = document.getElementById("display");
 var onClickNumberBtn = function (num) {
-    if (!calcItem.operator) {
-        calcItem.leftOperand = "".concat(calcItem.leftOperand).concat(num);
-        display.textContent = calcItem.leftOperand;
-    }
-    else if (calcItem.operator) {
+    if (calcItem.rightOperator) {
         calcItem.rightOperand = "".concat(calcItem.rightOperand).concat(num);
         display.textContent = calcItem.rightOperand;
+    }
+    else if (calcItem.leftOperator) {
+        calcItem.centerOperand = "".concat(calcItem.centerOperand).concat(num);
+        display.textContent = calcItem.centerOperand;
+    }
+    else if (!calcItem.leftOperator) {
+        calcItem.leftOperand = "".concat(calcItem.leftOperand).concat(num);
+        display.textContent = calcItem.leftOperand;
     }
 };
 var onClickPlusAndMinus = function (plusOrMinus) {
     if (display.textContent === null) {
         return;
     }
-    if (calcItem.operator === OPERATOR.PLUS || calcItem.operator === OPERATOR.MINUS) {
-        var totalStr = calcForString(calcItem.leftOperand, calcItem.rightOperand);
+    if (calcItem.rightOperator === OPERATOR.MULTIPLY) {
+        calcItem.centerOperand = calcForString(calcItem.rightOperator, calcItem.centerOperand, calcItem.rightOperand);
+        var totalStr = calcForString(calcItem.leftOperator, calcItem.leftOperand, calcItem.centerOperand);
         display.textContent = totalStr;
         calcItem = {
             leftOperand: totalStr,
-            operator: plusOrMinus,
-            rightOperand: ""
+            centerOperand: "",
+            rightOperand: "",
+            leftOperator: plusOrMinus,
+            rightOperator: ""
         };
     }
+    else if (calcItem.leftOperator === OPERATOR.PLUS || calcItem.leftOperator === OPERATOR.MINUS || calcItem.leftOperator === OPERATOR.MULTIPLY) {
+        var totalStr = calcForString(calcItem.leftOperator, calcItem.leftOperand, calcItem.centerOperand);
+        display.textContent = totalStr;
+        calcItem = __assign(__assign({}, calcItem), { leftOperand: totalStr, centerOperand: "", leftOperator: plusOrMinus });
+    }
     else if (!calcItem.leftOperand) {
-        calcItem = __assign(__assign({}, calcItem), { leftOperand: display.textContent, operator: plusOrMinus });
+        calcItem = __assign(__assign({}, calcItem), { leftOperand: display.textContent, leftOperator: plusOrMinus });
     }
     else {
-        calcItem.operator = plusOrMinus;
+        calcItem.leftOperator = plusOrMinus;
+    }
+};
+var onClickMultiply = function (multiply) {
+    if (display.textContent === null) {
+        return;
+    }
+    if (calcItem.rightOperator === OPERATOR.MULTIPLY) {
+        var totalStr = calcForString(calcItem.rightOperator, calcItem.centerOperand, calcItem.rightOperand);
+        display.textContent = totalStr;
+        calcItem = __assign(__assign({}, calcItem), { centerOperand: totalStr, rightOperand: "", rightOperator: multiply });
+    }
+    else if (calcItem.leftOperator === OPERATOR.MULTIPLY) {
+        var totalStr = calcForString(calcItem.leftOperator, calcItem.leftOperand, calcItem.centerOperand);
+        display.textContent = totalStr;
+        calcItem = __assign(__assign({}, calcItem), { leftOperand: totalStr, centerOperand: "", leftOperator: multiply });
+    }
+    else if (calcItem.leftOperator === OPERATOR.PLUS || calcItem.leftOperator === OPERATOR.MINUS) {
+        display.textContent = calcItem.centerOperand;
+        calcItem = __assign(__assign({}, calcItem), { rightOperator: multiply });
+    }
+    else if (!calcItem.leftOperand) {
+        calcItem = __assign(__assign({}, calcItem), { leftOperand: display.textContent, leftOperator: OPERATOR.MULTIPLY });
+    }
+    else {
+        calcItem.leftOperator = OPERATOR.MULTIPLY;
     }
 };
 var onClickEqual = function () {
-    if (calcItem.leftOperand && calcItem.operator) {
-        var totalStr = calcForString(calcItem.leftOperand, calcItem.rightOperand);
+    if (calcItem.rightOperand) {
+        calcItem.centerOperand = calcForString(calcItem.rightOperator, calcItem.centerOperand, calcItem.rightOperand);
+        var totalStr = calcForString(calcItem.leftOperator, calcItem.leftOperand, calcItem.centerOperand);
         display.textContent = totalStr;
         calcItem = {
             leftOperand: "",
-            operator: "",
-            rightOperand: ""
+            centerOperand: "",
+            rightOperand: "",
+            leftOperator: "",
+            rightOperator: ""
+        };
+    }
+    else if (calcItem.centerOperand && !calcItem.rightOperator) {
+        var totalStr = calcForString(calcItem.leftOperator, calcItem.leftOperand, calcItem.centerOperand);
+        display.textContent = totalStr;
+        calcItem = {
+            leftOperand: "",
+            centerOperand: "",
+            rightOperand: "",
+            leftOperator: "",
+            rightOperator: ""
         };
     }
 };
 var onClickClear = function () {
     if (calcItem.rightOperand) {
         calcItem.rightOperand = "";
+        display.textContent = calcItem.centerOperand;
+    }
+    else if (calcItem.centerOperand && !calcItem.rightOperator) {
+        calcItem.centerOperand = "";
         display.textContent = calcItem.leftOperand;
     }
     else {
-        calcItem = __assign(__assign({}, calcItem), { leftOperand: "", operator: "" });
+        calcItem = __assign(__assign({}, calcItem), { leftOperand: "", centerOperand: "", leftOperator: "", rightOperator: "" });
         display.textContent = "0";
     }
 };
-var calcForString = function (leftOperand, rightOperand) {
-    if (calcItem.operator === OPERATOR.PLUS) {
+var calcForString = function (operator, leftOperand, rightOperand) {
+    if (operator === OPERATOR.PLUS) {
         return "".concat(Number(leftOperand) + Number(rightOperand));
     }
-    else if (calcItem.operator === OPERATOR.MINUS) {
+    else if (operator === OPERATOR.MINUS) {
         return "".concat(Number(leftOperand) - Number(rightOperand));
+    }
+    else if (operator === OPERATOR.MULTIPLY) {
+        return "".concat(Number(leftOperand) * Number(rightOperand));
     }
     else {
         return "";
@@ -84,5 +145,5 @@ var calcForString = function (leftOperand, rightOperand) {
 };
 // 確認用
 function check() {
-    console.log("".concat(calcItem.leftOperand).concat(calcItem.operator).concat(calcItem.rightOperand));
+    console.log("".concat(calcItem.leftOperand).concat(calcItem.leftOperator).concat(calcItem.centerOperand).concat(calcItem.rightOperator).concat(calcItem.rightOperand));
 }
--- Calculator/src/main.ts
+++ Calculator/src/main.ts
@@ -1,64 +1,136 @@
 enum OPERATOR {
   PLUS = "plus",
-  MINUS = "minus" 
+  MINUS = "minus",
+  MULTIPLY = "multiply"
 }
 
-// [leftOperand] [operator(+,-)] [rightOperand] = displayの式をイメージしています。
-type Operator = "plus" | "minus" | "";
+// [leftOperand] [leftOperator(+,-,×)] [centerOperand] [rightOperator(+,-,×)] [rightOperand] = displayの式をイメージしています。
+type Operator = "plus" | "minus" | "multiply" | "";
 type CalcItem = {
   leftOperand: string;
+  centerOperand: string;
   rightOperand: string;
-  operator: Operator;
+  leftOperator: Operator;
+  rightOperator: Operator;
 }
 let calcItem: CalcItem = {
   leftOperand: "",
+  centerOperand: "",
   rightOperand: "",
-  operator: "",
+  leftOperator: "",
+  rightOperator: ""
 }
 
 const display: HTMLElement = <HTMLElement>document.getElementById("display");
 
 const onClickNumberBtn = (num: string): void => {
-  if(!calcItem.operator) {
-    calcItem.leftOperand = `${calcItem.leftOperand}${num}`;
-    display.textContent = calcItem.leftOperand;
-  } else if(calcItem.operator) {
+  if(calcItem.rightOperator) {
     calcItem.rightOperand = `${calcItem.rightOperand}${num}`;
     display.textContent = calcItem.rightOperand;
+  } else if(calcItem.leftOperator) {
+    calcItem.centerOperand = `${calcItem.centerOperand}${num}`;
+    display.textContent = calcItem.centerOperand;
+  } else if(!calcItem.leftOperator) {
+    calcItem.leftOperand = `${calcItem.leftOperand}${num}`;
+    display.textContent = calcItem.leftOperand;
   }
 }
 
 const onClickPlusAndMinus = (plusOrMinus: OPERATOR.PLUS | OPERATOR.MINUS): void => {
   if(display.textContent === null) {
     return;
-  }  
-  if(calcItem.operator === OPERATOR.PLUS || calcItem.operator === OPERATOR.MINUS) {   
-    const totalStr = calcForString(calcItem.leftOperand, calcItem.rightOperand);
+  }
+  if(calcItem.rightOperator === OPERATOR.MULTIPLY) {
+    calcItem.centerOperand = calcForString(calcItem.rightOperator,calcItem.centerOperand, calcItem.rightOperand);
+    const totalStr = calcForString(calcItem.leftOperator,calcItem.leftOperand, calcItem.centerOperand);
     display.textContent = totalStr;
     calcItem = {
       leftOperand: totalStr,
-      operator: plusOrMinus,
+      centerOperand: "",
       rightOperand: "",
+      leftOperator: plusOrMinus,
+      rightOperator: ""
+    }
+  } else if(calcItem.leftOperator === OPERATOR.PLUS || calcItem.leftOperator === OPERATOR.MINUS || calcItem.leftOperator === OPERATOR.MULTIPLY) {   
+    const totalStr = calcForString(calcItem.leftOperator, calcItem.leftOperand, calcItem.centerOperand);
+    display.textContent = totalStr;
+    calcItem = {
+      ...calcItem,
+      leftOperand: totalStr,
+      centerOperand: "",
+      leftOperator: plusOrMinus
     }
   } else if(!calcItem.leftOperand) {
     calcItem = {
       ...calcItem,
       leftOperand: display.textContent,
-      operator: plusOrMinus,
+      leftOperator: plusOrMinus
     }
   } else {
-    calcItem.operator = plusOrMinus;
+    calcItem.leftOperator = plusOrMinus;
+  }
+}
+
+const onClickMultiply = (multiply: OPERATOR.MULTIPLY): void => {
+  if(display.textContent === null) {
+    return;
+  }
+  if(calcItem.rightOperator === OPERATOR.MULTIPLY) {
+    const totalStr = calcForString(calcItem.rightOperator, calcItem.centerOperand, calcItem.rightOperand);
+    display.textContent = totalStr;
+    calcItem = {
+      ...calcItem,
+      centerOperand: totalStr,
+      rightOperand: "",
+      rightOperator: multiply
+    }
+  } else if(calcItem.leftOperator === OPERATOR.MULTIPLY) {
+    const totalStr = calcForString(calcItem.leftOperator, calcItem.leftOperand, calcItem.centerOperand);
+    display.textContent = totalStr;
+    calcItem = {
+      ...calcItem,
+      leftOperand: totalStr,
+      centerOperand: "",
+      leftOperator: multiply
+    }
+  } else if(calcItem.leftOperator === OPERATOR.PLUS || calcItem.leftOperator === OPERATOR.MINUS) {   
+    display.textContent = calcItem.centerOperand;
+    calcItem = {
+      ...calcItem,
+      rightOperator: multiply
+    }
+  } else if(!calcItem.leftOperand) {
+    calcItem = {
+      ...calcItem,
+      leftOperand: display.textContent,
+      leftOperator: OPERATOR.MULTIPLY
+    }
+  } else {
+    calcItem.leftOperator = OPERATOR.MULTIPLY;
   }
 }
 
 const onClickEqual = (): void => {
-  if(calcItem.leftOperand && calcItem.operator) {
-    const totalStr = calcForString(calcItem.leftOperand, calcItem.rightOperand);
+  if(calcItem.rightOperand) {
+    calcItem.centerOperand = calcForString(calcItem.rightOperator,calcItem.centerOperand, calcItem.rightOperand);
+    const totalStr = calcForString(calcItem.leftOperator,calcItem.leftOperand, calcItem.centerOperand);
     display.textContent = totalStr;
     calcItem = {
       leftOperand: "",
-      operator: "",
+      centerOperand: "",
       rightOperand: "",
+      leftOperator: "",
+      rightOperator: ""
+    }
+  } else if(calcItem.centerOperand && !calcItem.rightOperator) {
+    const totalStr = calcForString(calcItem.leftOperator,calcItem.leftOperand, calcItem.centerOperand);
+    display.textContent = totalStr;
+    calcItem = {
+      leftOperand: "",
+      centerOperand: "",
+      rightOperand: "",
+      leftOperator: "",
+      rightOperator: ""
     }
   }
 }
@@ -66,22 +138,28 @@ const onClickEqual = (): void => {
 const onClickClear = (): void => {
   if(calcItem.rightOperand) {
     calcItem.rightOperand = "";
+    display.textContent = calcItem.centerOperand;
+  } else if(calcItem.centerOperand && !calcItem.rightOperator) {
+    calcItem.centerOperand = "";
     display.textContent = calcItem.leftOperand;
   } else {
     calcItem = {
       ...calcItem,
       leftOperand: "",
-      operator: "",
+      centerOperand: "",
+      leftOperator: "",
+      rightOperator: ""
     }
     display.textContent = "0";
   }
 }
-
-const calcForString = (leftOperand: string, rightOperand: string): string => {
-  if(calcItem.operator === OPERATOR.PLUS) {
+const calcForString = (operator: Operator, leftOperand: string, rightOperand: string): string => {
+  if(operator === OPERATOR.PLUS) {
     return `${Number(leftOperand) + Number(rightOperand)}`;
-  } else if(calcItem.operator === OPERATOR.MINUS) {
+  } else if(operator === OPERATOR.MINUS) {
     return `${Number(leftOperand) - Number(rightOperand)}`;
+  } else if(operator === OPERATOR.MULTIPLY) {
+    return `${Number(leftOperand) * Number(rightOperand)}`;
   } else {
     return "";
   } 
@@ -89,5 +167,5 @@ const calcForString = (leftOperand: string, rightOperand: string): string => {
 
 // 確認用
 function check(): void {
-  console.log(`${calcItem.leftOperand}${calcItem.operator}${calcItem.rightOperand}`);
+  console.log(`${calcItem.leftOperand}${calcItem.leftOperator}${calcItem.centerOperand}${calcItem.rightOperator}${calcItem.rightOperand}`);
 }
